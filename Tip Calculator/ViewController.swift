313de0065b4e1942ed40139b5a69d8e916556f9d
ThangLai <ronaldocr9803@gmail.com>
2020-12-03 10:21:50 +0700

    add logic for tip calculation

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,14 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstPercentTextfield: UITextField!
+    @IBOutlet weak var secondPercentTextfield: UITextField!
+    @IBOutlet weak var thirdPercentTextfield: UITextField!
+    @IBOutlet weak var totalBillTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstPercentTextfield.delegate = self
+        secondPercentTextfield.delegate = self
+        thirdPercentTextfield.delegate = self
+        totalBillTextField.delegate = self
+
+        totalBillTextField.isUserInteractionEnabled = false
+        
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func calculateTipsPressed(_ sender: Any) {
+        var totalBill: Float = 0.0
+        
+        let firstBill = firstPercentTextfield.text!.isEmpty ? "0" : firstPercentTextfield.text!
+        let secondBill = secondPercentTextfield.text!.isEmpty ? "0" : secondPercentTextfield.text!
+        let thirdBill = thirdPercentTextfield.text!.isEmpty ? "0" : thirdPercentTextfield.text!
+        
+        totalBill = Float(firstBill)! * 1.2 + Float(secondBill)! * 1.1 + Float(thirdBill)! * 1.1
+        print(totalBill)
+        totalBillTextField.text = String(format: "$ %.2f", totalBill) //"$ \(totalBill)"
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -25,5 +51,15 @@ class ViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.firstPercentTextfield.resignFirstResponder()
+        self.secondPercentTextfield.resignFirstResponder()
+        self.thirdPercentTextfield.resignFirstResponder()
+        return true
+    }
 
 }
